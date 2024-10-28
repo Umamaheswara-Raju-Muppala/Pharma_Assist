4d328b11862e49bdf6db4446ca8c2a0a3055f9e8
Umamaheswara-Raju-Muppala <mahidarling678@gmail.com>
2024-10-28 12:04:06 +0530

    Project Started and Initialized

---
+++ src/test/java/com/pharma_assist/PharmaAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharma_assist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
